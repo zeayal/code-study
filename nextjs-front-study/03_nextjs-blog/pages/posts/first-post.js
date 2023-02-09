75d300dbed1c8f4915e14a423b5d984d36c505d9
ziyang.liu <ziyang.liu@mobilemd.cn>
2023-02-09 23:52:16 +0800

    feature: getStaticProps

--- nextjs-front-study/03_nextjs-blog/pages/posts/first-post.js
+++ nextjs-front-study/03_nextjs-blog/pages/posts/first-post.js
@@ -1,15 +1,31 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Head from "next/head";
+import Script from "next/script";
+import Layout from "../../components/layout";
+import Alert from "../../components/alert";
 
 export default function FirstPost() {
   return (
-    <>
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() => {
+          console.log("script loaded");
+        }}
+      />
       <h1>First Post</h1>
+      <Alert type="success" >success</Alert>
+      <Alert type="error">error</Alert>
       <h2>
         <Link href="/">Back to home</Link>
       </h2>
-      <h2>
-        <a href='/'>Back to home with a tag</a>
+      <h2 className="mt-6">
+        <a href="/">Back to home with a tag</a>
       </h2>
-    </>
+    </Layout>
   );
 }
---
+++ nextjs-front-study/03_nextjs-blog/tailwind.config.js
@@ -0,0 +1,22 @@
+// module.exports = {
+//     content: [
+//         './pages/**/*.{js,ts,jsx,tsx}',
+//         './component/**/*.{js,ts,jsx,tsx}'
+//     ]
+// }
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx}",
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+   
+      // Or if using `src` directory:
+      "./src/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
